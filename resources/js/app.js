3505fd13c9cd6a4763f7632d74d6ec399189a76d
efren <efren.hernandez@alumnos.udg.mx>
2021-06-30 14:48:56 -0500

    Added TokenComponent to app and bootstrap-vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,7 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
-
+Vue.use(require('bootstrap-vue'));
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -20,6 +20,7 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('token-component', require('./components/TokenComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
