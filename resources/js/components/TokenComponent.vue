5b42e9e3a8dd96b840802e746a902da0b17f0f2e
efren <efren.hernandez@alumnos.udg.mx>
2021-06-30 16:16:36 -0500

    Changed TokenComponent
    Added form with bootstrap-vue form

--- resources/js/components/TokenComponent.vue
+++ resources/js/components/TokenComponent.vue
@@ -4,20 +4,27 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Git token</div>
- 
+                    <div class="card-header">Set git token</div>
                     <div class="card-body">
-                        <form @submit="formSubmit">
-                        <strong>Token:</strong>
-                        <textarea class="form-control" v-model="description"></textarea>
-                        <button class="btn btn-success">Submit</button>
-                        </form>
-                        <pre>
-                        {{output}}
-                        </pre>
-                        <h2>Token</h2>
-  <strong v-if="token">{{ token }}</strong>
-  <strong v-else>No Token? Click<a href="https://docs.github.com/en/github/authenticating-to-github/keeping-your-account-and-data-secure/creating-a-personal-access-token"> here </a>to learn how to make token</strong>
+                        <b-form @submit="formSubmit">
+                            <b-form-group
+                                id="input-group-1"
+                                label="Token:"
+                                label-for="input-1"
+                                description="Insert a generated git token">
+                                <b-form-input
+                                id="input-1"
+                                v-model="form.token"
+                                placeholder=""
+                                required
+                                ></b-form-input>
+                            </b-form-group>
+                            
+                            <b-button type="submit" variant="primary">Submit</b-button>
+                        </b-form>
+                        <h2>Registered token</h2>
+                        <strong v-if="token">{{ token }}</strong>
+                        <strong v-else>No Token? Click<a href="https://docs.github.com/en/github/authenticating-to-github/keeping-your-account-and-data-secure/creating-a-personal-access-token"> here </a>to learn how to make token</strong>
                     </div>
                 </div>
             </div>
@@ -40,10 +47,13 @@
         },
         data() {
             return {
+                form: {
+                token: ''
+                },
               info: null,
               name: '',
               description: '',
-              output: '',
+              output: null,
               token: null
             };
         },
@@ -52,20 +62,23 @@
               e.preventDefault()
                 let currentObj = this;
                 
-                axios.post('/saveToken',{token: this.description})
+                axios.post('/saveToken',{token: currentObj.form.token})
                 .then(function (response) {
-                    currentObj.output = response.data;
-                    axios.post('/getToken')
-                    .then(function (response) {
-                        currentObj.token = response.data;
-                    })
-                    .catch(function (error) {
-                        currentObj.output = error;
-                    });
-                    })
+                    window.alert(response.data)
+                axios.post('/getToken')
+                .then(function (response) {
+                    currentObj.token = response.data;
+                })
                 .catch(function (error) {
+                    window.alert(error)
                     currentObj.output = error;
                 });
+                })
+                .catch(function (error) {
+                    window.alert(error)
+                    currentObj.output = error;
+                });
+               
             }
         }
     }
